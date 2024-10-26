e29f719b26512273cca4c25a4e497538138f862b
Amirzhan Albariyev <albariev123@gmail.com>
2024-10-26 17:03:42 +0300

    Withdrawn if main / def main

--- publication_all_photo.py
+++ publication_all_photo.py
@@ -5,31 +5,35 @@ import random
 import os
 import argparse
 
-env = Env()
-env.read_env()
 
-bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
-chat_id = '@doubletesttg'
-images_path = 'images'
+def main():
+    env = Env()
+    env.read_env()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--PUBLICATION_INTERVAL', type=int, help='Интервал для публикаций(в сек.)')
-args = parser.parse_args()
-publocation_interval = args.PUBLICATION_INTERVAL
-all_files = []
+    bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
+    chat_id = '@doubletesttg'
+    images_path = 'images'
 
-if publocation_interval==None:
-    publocation_interval = env.int('PUBLICATION_INTERVAL')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--PUBLICATION_INTERVAL', type=int, help='Интервал для публикаций(в сек.)')
+    args = parser.parse_args()
+    publocation_interval = args.PUBLICATION_INTERVAL
+    all_files = []
 
-for dirpath, dirnames, filenames in os.walk(images_path):
-    for file in filenames:
-        file_path = os.path.join(dirpath, file)
-        all_files.append(file_path)
+    if publocation_interval==None:
+        publocation_interval = env.int('PUBLICATION_INTERVAL')
 
-while True:
-    random.shuffle(all_files)
-    for file in all_files:
-        with open(file, 'rb') as image:
-            bot.send_document(chat_id=chat_id, document=image)
-        time.sleep(publocation_interval)
+    for dirpath, dirnames, filenames in os.walk(images_path):
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            all_files.append(file_path)
 
+    while True:
+        random.shuffle(all_files)
+        for file in all_files:
+            with open(file, 'rb') as image:
+                bot.send_document(chat_id=chat_id, document=image)
+            time.sleep(publocation_interval)
+
+if __name__ == '__main__':
+    main()
--- publication_one_photo.py
+++ publication_one_photo.py
@@ -4,31 +4,36 @@ import random
 import os
 import argparse
 
-env = Env()
-env.read_env()
 
-bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
-chat_id = '@doubletesttg'
-images_path = 'images'
+def main():
+    env = Env()
+    env.read_env()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file_number', type=int, help='Какую фотографию опубликовать?')
-args = parser.parse_args()
-file_number = args.file_number
-all_files = []
+    bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
+    chat_id = '@doubletesttg'
+    images_path = 'images'
 
-for dirpath, dirnames, filenames in os.walk(images_path):
-    for file in filenames:
-        file_path = os.path.join(dirpath, file)
-        all_files.append(file_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_number', type=int, help='Какую фотографию опубликовать?')
+    args = parser.parse_args()
+    file_number = args.file_number
+    all_files = []
 
-if file_number:
-    file = all_files[file_number - 1]
-else:
-    file = random.choice(all_files)
+    for dirpath, dirnames, filenames in os.walk(images_path):
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            all_files.append(file_path)
 
-with open(file, 'rb') as image:
-    bot.send_document(chat_id=chat_id, document=image)
+    if file_number:
+        file = all_files[file_number - 1]
+    else:
+        file = random.choice(all_files)
+
+    with open(file, 'rb') as image:
+        bot.send_document(chat_id=chat_id, document=image)
+
+if __name__ == '__main__':
+    main()
 
     
         
