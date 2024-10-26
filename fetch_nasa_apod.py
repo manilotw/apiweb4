fd851f05dbcd981aa494107b1e40c3942ec822c8
Amirzhan Albariyev <albariev123@gmail.com>
2024-10-26 16:39:11 +0300

    variable name changed

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -8,13 +8,13 @@ from environs import Env
 env = Env()
 env.read_env()
 
-def fetch_apod_images(count, nasa_api):
+def fetch_apod_images(count, nasa_api_key):
     url = env.str('API_NASA_APOD')
 
     for number in range(1, count+1):
 
         params = {
-            'api_key': nasa_api,
+            'api_key': nasa_api_key,
             'count': number
         }
 
@@ -30,9 +30,9 @@ def fetch_apod_images(count, nasa_api):
         download_picture(filename, photo_url)
 
 def main():
-    nasa_api = env.str('NASA_TOKEN')
+    nasa_api_key = env.str('NASA_TOKEN')
 
-    fetch_apod_images(30, nasa_api)
+    fetch_apod_images(30, nasa_api_key)
 
 if __name__ == '__main__':
     main()
